920c10096c91244ef90d4080322ba1cf95189c7b
Korhan Mutludoğan <korhanmd@gmail.com>
2022-11-21 16:01:28 +0300

    Define methods and attributes related to TrafficLightPhase

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -42,6 +42,7 @@ public:
     TrafficLight();
 
     // getters / setters
+    TrafficLightPhase getCurrentPhase();
 
     // typical behaviour methods
     void waitForGreen();
@@ -50,6 +51,8 @@ public:
 private:
     // typical behaviour methods
     void cycleThroughPhases();
+
+    TrafficLightPhase _currentPhase;
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
